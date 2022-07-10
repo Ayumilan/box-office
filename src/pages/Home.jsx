dddef1dd2d2151f29a5785942ec2529b4b846a9a
Ayush Milan <ayushmilan000@gmail.com>
2022-07-10 12:43:50 +0530

    added nav bar and created page components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>This is Home</div>
+  )
+}
+
+export default Home
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react'
+
+const Starred = () => {
+  return (
+    <div>
+      This is Starred
+    </div>
+  )
+}
+
+export default Starred
