2830d2ea21063ee51dc92f131d9dfedd74aaec02
Ayush Milan <ayushmilan000@gmail.com>
2022-07-10 14:41:10 +0530

    created preview cards for api data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable */
 import React, { useState } from 'react'
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout'
+import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
@@ -38,9 +40,8 @@ const Home = () => {
     }
     if(results && results.length > 0) {
       return results[0].show ? 
-      results.map(  (item) => <div key={item.show.id}>{item.show.name}</div> ) : 
-      results.map(  (item) => <div key={item.person.id}>{item.person.name}</div> )
-
+      <ShowGrid data={results}/> : 
+      <ActorGrid data={results} />;
     }
     return null;
   }
